858a614af5466974e1ac2f67363489db7ae75d94
ikazame <komada@mibel.cs.tsukuba.ac.jp>
2020-08-27 18:51:51 +0900

    サーバと通信して変換できるようにしたりした

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, createRef} from 'react';
 import './App.css';
 import {KEY} from './keyboard_code_enums';
+import { toKana } from 'wanakana';
 
 const InputState = {
-  Inputing: 0,
-  Converting: 1
+  Waiting: 0,
+  Inputing: 1,
+  Converting: 2
 }
 
 function useIMEState(addText) {
   const [rawInput, setRawInput] = useState('');
-  const [inputState, setInputState] = useState(InputState.Inputing);
+  const [inputState, setInputState] = useState(InputState.Waiting);
   // let candidates = ['東京', '東響', '問う京']
   const [candidates, setCandidates] = useState([])
   const [candix, setCandix] = useState(0);
@@ -17,58 +19,104 @@ function useIMEState(addText) {
   // setRawInput(candidates[candix])
 
   useEffect(() =>{
-    if(inputState == InputState.Converting) {
+    if(inputState === InputState.Converting) {
       setRawInput(candidates[candix])
+      // 非同期通信がいつ終わるかわからないので、ここでは面倒見れない
     }
-  }, [inputState, candix]);
+  }, [candix]);
 
   function onKey(e){
     let keyCode = e.keyCode;
-    if(e.keyCode == KEY.BACKSPACE) {
+    if (inputState === InputState.Waiting) {
+      // command/ctrlが同時押しされているときもreturnでいい
+      if(!((48 <= e.keyCode && e.keyCode <= 90) || (186 <= e.keyCode && e.keyCode <= 191)
+    || (219 <= e.keyCode && e.keyCode <= 222))) 
+        // 新規の文字追加入力でなければ、onChangeに任せる(カーソル移動など)
+        return;
+    }
+    if(e.keyCode === KEY.BACKSPACE) {
       setRawInput(rawInput.substring(0, rawInput.length-1));
     }
-    else if(e.keyCode == KEY.SPACE) {
-      if(inputState == InputState.Inputing) {
+    else if(e.keyCode === KEY.SPACE) {
+      if(inputState === InputState.Inputing) {
         setInputState(InputState.Converting)
         // mock
-        setCandidates(['東京', '東響', '問う京'])
+        // setCandidates(['東京', '東響', '問う京'])
+
+        const obj = {input: rawInput};
+        const method = "POST";
+        const body = JSON.stringify(obj);
+        const headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        };
+        fetch("http://localhost:5000", {method, headers, body})
+        .then((res) => res.text()
+        .then((text) => {
+          let results = JSON.parse(text);
+          setCandidates(results);
+          setRawInput(results[0]);
+          setCandix(0)
+        }));
       }
-      else if(inputState == InputState.Converting) {
+      else if(inputState === InputState.Converting) {
         setCandix((candix+1) % candidates.length);
       }
     }
-    else if(e.keyCode == KEY.DOWN_ARROW && inputState == InputState.Converting) {
+    else if(e.keyCode === KEY.DOWN_ARROW && inputState === InputState.Converting) {
       setCandix((candix+1) % candidates.length);
     }
-    else if(e.keyCode == KEY.UP_ARROW&& inputState == InputState.Converting) {
+    else if(e.keyCode === KEY.UP_ARROW&& inputState === InputState.Converting) {
       setCandix((candix-1+candidates.length) % candidates.length);
     }
-    else if(e.keyCode == KEY.ENTER) {
-      addText(candidates[candix])
+    else if(e.keyCode === KEY.ENTER) {
+      if(inputState === InputState.Inputing){
+        addText(rawInput)
+      }
+      else if(inputState === InputState.Converting){
+        addText(candidates[candix])
+      }
       setRawInput('')
-      setInputState(InputState.Inputing);
+      setInputState(InputState.Waiting);
     }
     else if((48 <= e.keyCode && e.keyCode <= 90) || (186 <= e.keyCode && e.keyCode <= 191)
     || (219 <= e.keyCode && e.keyCode <= 222)) {
-      setRawInput(rawInput + e.key);
+      setInputState(InputState.Inputing)
+      // nをすぐ"ん"にしようとするので、その対策
+      if(e.keyCode === KEY.KEY_N)
+        setRawInput(rawInput + e.key);
+      else
+        setRawInput(toKana(rawInput + e.key));
     }
+    e.preventDefault();
   }
   return [rawInput, inputState, candidates, candix, onKey];
 }
 
-function useTextarea() {
+function useTextarea(_textareaRef) {
   const [text, setText] = useState('');
-  
+  const textareaRef = _textareaRef;
+
+  useEffect(() =>{
+    // textareaRef.current.selectionStart = 0
+    // textareaRef.current.selectionEnd = 0
+  }, [text]);
+
   function addText(subtext) {
-    setText(text + subtext)
+    // https://qiita.com/noraworld/items/d6334a4f9b07792200a5
+    let pos = textareaRef.current.selectionStart
+    setText(text.substr(0, pos) + subtext + text.substr(pos, text.length))
+
+    textareaRef.current.selectionStart = 0
   }
 
-  return [text, addText];
+  return [text, addText, setText];
 }
 
 function App() {
   const [mode, setMode] = useState(false);
-  const [text, addText] = useTextarea();
+  const textareaRef = createRef()
+  const [text, addText, setText] = useTextarea(textareaRef);
   const [preEdit, inputState, candidates, candix, onKey] = useIMEState(addText);
 
   return (
@@ -77,22 +125,23 @@ function App() {
         <button onClick={() => setMode(!mode)}>{mode ? 'on' : 'off'}</button>
       </div>
         <textarea className={mode ? "imeOn" : "imeOff"}
-      onKeyDown={(e) => {onKey(e); e.preventDefault();}} value={text}>
+      onKeyDown={(e) => {if(mode) onKey(e);}} onChange={(e) => {setText(e.target.value)}} value={text} ref={textareaRef}>
       </textarea>
       <div>
-        <input type="text" style={{textDecoration:"underline"}} value={preEdit} readOnly={true} />
+        <input type="text" style={{textDecoration:"underline"}} value={preEdit} readOnly={true} id={"preEdit"} />
       </div>
       {
-        inputState == InputState.Converting &&  
+        inputState === InputState.Converting &&  
         <ul>
           {candidates.map((item, i) =>
-            <li key={item} className={i == candix ? "selecting" : ""}>{item}</li>
+            <li key={item} className={i === candix ? "selecting" : ""}>{item}</li>
           )}
         </ul>
       }
     </div>
-
   );
 }
 
 export default App;
+
+// event.target.valueにテキストエリアを変更したあとの値が入るなんて知らなかったのだな
