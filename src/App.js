954ddeaafe80675aacd00d63404871b207ef7bf7
ikazame <komada@mibel.cs.tsukuba.ac.jp>
2020-08-28 16:51:45 +0900

    リファクタリング

--- src/App.js
+++ src/App.js
@@ -9,43 +9,38 @@ const InputState = {
   Converting: 2
 }
 
+function isNormalKey(keyCode) {
+  return (48 <= keyCode && keyCode <= 90) || (186 <= keyCode && keyCode <= 191)
+    || (219 <= keyCode && keyCode <= 222)
+}
+
 function useIMEState(preEditRef, addText) {
-  const [rawInput, setRawInput] = useState('');
+  const [preEdit, setPreEdit] = useState('');
   const [inputState, setInputState] = useState(InputState.Waiting);
   // let candidates = ['東京', '東響', '問う京']
   const [candidates, setCandidates] = useState([])
   const [candix, setCandix] = useState(0);
   // candixが変更されるときにこれで変えて
-  // setRawInput(candidates[candix])
+  // setPreEdit(candidates[candix])
 
   useEffect(() =>{
     if(inputState === InputState.Converting) {
-      setRawInput(candidates[candix])
-      // 非同期通信がいつ終わるかわからないので、ここでは面倒見れない
+      setPreEdit(candidates[candix])
     }
   }, [candix]);
 
   useEffect(() => {
     if(inputState === InputState.Inputing){
       preEditRef.current.focus();
-      // document.getSelection().collapse(preEditRef.current, 1)
-      // preEditRef.current.setSelectionRange(1, 1);
-      console.log("focused")
     } 
   }, [inputState])
 
   function onKey(e){
-    let keyCode = e.keyCode;
-    if (inputState === InputState.Waiting) {
-      // command/ctrlが同時押しされているときもreturnでいい
-      if(!((48 <= e.keyCode && e.keyCode <= 90) || (186 <= e.keyCode && e.keyCode <= 191)
-    || (219 <= e.keyCode && e.keyCode <= 222))) 
-        // 新規の文字追加入力でなければ、onChangeに任せる(カーソル移動など)
-        return;
-      if(e.ctrlKey) return;
+    if (inputState === InputState.Waiting && e.ctrlKey){
+      return;
     }
-    if(e.keyCode === KEY.BACKSPACE && inputState !== InputState.Converting) {
-      setRawInput(rawInput.substring(0, rawInput.length-1));
+    else if(e.keyCode === KEY.BACKSPACE && inputState === InputState.Converting) {
+      e.preventDefault()
     }
     else if(e.keyCode === KEY.SPACE) {
       if(inputState === InputState.Inputing) {
@@ -54,11 +49,11 @@ function useIMEState(preEditRef, addText) {
           let results = ['東京', '東響', '問う京']
           setInputState(InputState.Converting)
           setCandidates(results);
-          setRawInput(results[0]);
+          setPreEdit(results[0]);
           setCandix(0)
         }
         else{
-          const obj = {input: rawInput};
+          const obj = {input: preEdit};
           const method = "POST";
           const body = JSON.stringify(obj);
           const headers = {
@@ -71,7 +66,7 @@ function useIMEState(preEditRef, addText) {
             let results = JSON.parse(text);
             setInputState(InputState.Converting)
             setCandidates(results);
-            setRawInput(results[0]);
+            setPreEdit(results[0]);
             setCandix(0)
           }));
         }
@@ -79,50 +74,41 @@ function useIMEState(preEditRef, addText) {
       else if(inputState === InputState.Converting) {
         setCandix((candix+1) % candidates.length);
       }
+      e.preventDefault();
     }
     else if(e.keyCode === KEY.DOWN_ARROW && inputState === InputState.Converting) {
       setCandix((candix+1) % candidates.length);
+      e.preventDefault();
     }
     else if(e.keyCode === KEY.UP_ARROW&& inputState === InputState.Converting) {
       setCandix((candix-1+candidates.length) % candidates.length);
+      e.preventDefault();
     }
-    else if(e.keyCode === KEY.ENTER) {
-      if(inputState === InputState.Inputing){
-        addText(rawInput)
-      }
-      else if(inputState === InputState.Converting){
-        addText(candidates[candix])
-      }
-      setRawInput('')
+    else if(e.keyCode === KEY.ENTER && inputState !== InputState.Waiting) {
+      addText(preEdit)
+      setPreEdit('')
       setInputState(InputState.Waiting);
+      e.preventDefault();
     }
-    else if((48 <= e.keyCode && e.keyCode <= 90) || (186 <= e.keyCode && e.keyCode <= 191)
-    || (219 <= e.keyCode && e.keyCode <= 222)) {
-      if(inputState !== InputState.Converting){
-        if(inputState === InputState.Waiting){
-          setInputState(InputState.Inputing)
-        }
-        // nをすぐ"ん"にしようとするので、その対策
-        if(e.keyCode === KEY.KEY_N &&  rawInput[rawInput.length-1] !== 'n')
-          setRawInput(rawInput + e.key);
-        else if(e.keyCode === KEY.KEY_Y && rawInput[rawInput.length-1] === 'n')
-          setRawInput(rawInput + e.key);
-        else
-          setRawInput(toKana(rawInput + e.key));
+    else if(isNormalKey(e.keyCode) && inputState !== InputState.Converting) {
+      if(inputState === InputState.Waiting){
+        setInputState(InputState.Inputing)
       }
+      // nをすぐ"ん"にしようとするので、その対策
+      if(e.keyCode === KEY.KEY_N &&  preEdit[preEdit.length-1] !== 'n')
+        setPreEdit(preEdit + e.key);
+      else if(e.keyCode === KEY.KEY_Y && preEdit[preEdit.length-1] === 'n')
+        setPreEdit(preEdit + e.key);
+      else
+        setPreEdit(toKana(preEdit + e.key));
+      e.preventDefault();
     }
-    else if(e.keyCode === KEY.LEFT_ARROW || e.keyCode === KEY.RIGHT_ARROW){
-      // preventDefaultさせたくない。でもこの書き方はdirty
-      return;
-    }
-    e.preventDefault();
   }
-  return [rawInput, setRawInput, inputState, candidates, candix, onKey];
+  return [preEdit, setPreEdit, inputState, candidates, candix, onKey];
 }
 
-function useTextarea(_textareaRef) {
+function useTextarea(textareaRef) {
   const [text, setText] = useState('');
-  const textareaRef = _textareaRef;
   // 実際のカーソル位置と完全に同期しているわけではないので注意
   // 設定直後のみ値とカーソル位置が同一になることが保証される
   const [cursor, setCursor] = useState(0)
@@ -138,8 +124,6 @@ function useTextarea(_textareaRef) {
     let pos = textareaRef.current.selectionStart
     setText(text.substr(0, pos) + subtext + text.substr(pos, text.length))
     setCursor(pos+subtext.length)
-
-    // textareaRef.current.selectionStart = 0
   }
 
   function saveCursorPos() {
